44fc151d17a2825d79a0fb9dc8f29f0e23cb2eec
TalandBotirjon <85695336+TalandBotirjon@users.noreply.github.com>
2023-01-25 20:55:00 +0500

    commit initial

--- metsenat/urls.py
+++ metsenat/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('dashboard/', views.DashboardViews.as_view()),
     # Sponsor
     path('sponsor/', views.SponsorListViews.as_view()),
-    path('sponsor/<int:pk>/', views.SponsorDetailViews.as_view()),
+    path('sponsor/<int:pk>/detail/', views.SponsorDetailViews.as_view()),
     # Student
     path('student/', views.StudentListViews.as_view()),
     path('student/create/', views.StudentCreateViews.as_view()),
-    path('student/<int:pk>/', views.StudentViews.as_view()),
+    path('student/<int:pk>/detail/', views.StudentViews.as_view()),
     # Metsenat
     path('metsenat/', views.MentanetViews.as_view()),
 ]
